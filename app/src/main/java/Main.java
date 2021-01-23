2667b1d91354ccb838bfeba63ddda73d137a3f01
Piotr Peter <piotrekpeter2001@gmail.com>
2021-01-23 11:31:32 +0100

    set up fxml

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
+import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -10,6 +14,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.show();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/root.fxml"));
+            Scene scene = new Scene(root, 300, 275);
+            stage.setTitle("FXML Welcome");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
